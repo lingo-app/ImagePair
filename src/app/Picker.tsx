67d5962f69956d06fdc67aeca0b61f4389148d70
Brendan Morse <42715623+bmmorse@users.noreply.github.com>
2023-03-14 14:40:38 -0400

    add not commited pages

--- src/app/Picker.tsx
+++ src/app/Picker.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-"use client";
-
-import { Asset, Item, Kit } from "@lingo-app/node";
-import React, { useCallback, useState } from "react";
-
-type Props = {
-    characters: Asset[];
-    objects: Asset[];
-}
-const Picker: React.FC<Props> =({ characters, objects }) => {
-=======
 'use client';
 
 import { Asset, Item, Kit } from '@lingo-app/node';
@@ -21,54 +9,30 @@ type Props = {
 };
 
 const Picker: React.FC<Props> = ({ characters, objects }) => {
->>>>>>> 7f48b21 (first commit for UI update)
   const [character, setCharacter] = useState<Asset>();
   const [object, setObject] = useState<Asset>();
   const [savedItem, setSavedItem] = useState<Item>();
 
   const saveImage = useCallback(async () => {
-<<<<<<< HEAD
-
-    if (!character || !object) {
-        return;
-    }
-
-    const res = await fetch("/api/save", {
-      method: "POST",
-=======
     if (!character || !object) {
       return;
     }
 
     const res = await fetch('/api/save', {
       method: 'POST',
->>>>>>> 7f48b21 (first commit for UI update)
       body: JSON.stringify({
         character: character.id,
         object: object.id,
       }),
     });
 
-<<<<<<< HEAD
-    const json = await res.json()
-    console.log(json);
-    setSavedItem(json.item);
-
-=======
     const json = await res.json();
     console.log(json);
     setSavedItem(json.item);
->>>>>>> 7f48b21 (first commit for UI update)
   }, [character, object]);
 
   if (savedItem) {
     // If an image has been saved, show a success message and a link to the item
-<<<<<<< HEAD
-    return <>
-    <p>Saved item</p>
-    <a target="_blank" href={`https://lingo.lingoapp.com/a/${savedItem.id}`}>View in lingo</a>
-    </>
-=======
     return (
       <>
         <p>Saved item</p>
@@ -80,36 +44,10 @@ const Picker: React.FC<Props> = ({ characters, objects }) => {
         </a>
       </>
     );
->>>>>>> 7f48b21 (first commit for UI update)
   }
 
   return (
     <>
-<<<<<<< HEAD
-    <div style={{display: "flex"}}>
-      <div style={{display: "flex", flexDirection: "column"}}>
-        {characters.map((asset) => (
-          <button key={asset.id} onClick={() => setCharacter(asset)}>
-            {asset.name}
-          </button>
-        ))}
-        <p>Character: {character?.name}</p>
-      </div>
-      <div style={{display: "flex", flexDirection: "column"}}>
-        {objects.map((asset) => (
-          <button key={asset.id} onClick={() => setObject(asset)}>
-            {asset.name}
-          </button>
-        ))}
-        <p>Object: {object?.name}</p>
-      </div>
-    </div>
-    <button onClick={saveImage}>Save</button>
-    </>
-  );
-}
-export default Picker;
-=======
       <div style={{ display: 'flex' }}>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           {characters.map((asset) => {
@@ -139,4 +77,3 @@ export default Picker;
   );
 };
 export default Picker;
->>>>>>> 7f48b21 (first commit for UI update)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,14 @@
-<<<<<<< HEAD
-import React from "react";
-import styles from "./page.module.css";
-import lingo, { Asset, ItemType, Kit } from "@lingo-app/node";
-import Picker from "./Picker";
-
-lingo.setup(
-  Number(process.env.LINGO_SPACE_ID),
-  process.env.LINGO_API_KEY as string
-);
-
-async function getSectionAssets(id: string): Promise<Asset[]> {
-  const section = await lingo.fetchSection(id)
-=======
 import React from 'react';
 import lingo, { Asset, ItemType, Kit } from '@lingo-app/node';
 import App from './App';
 
 async function getSectionAssets(id: string): Promise<Asset[]> {
   const section = await lingo.fetchSection(id);
->>>>>>> 7f48b21 (first commit for UI update)
   return section.items
     .filter((item) => item.type === ItemType.Asset)
     .map((item) => item.asset) as Asset[];
 }
 
-<<<<<<< HEAD
-export default async function Home() {
-  const characters = await getSectionAssets("RG72q5")
-  const objects = await getSectionAssets("Pww1EY")
-
-  return (
-    <main className={styles.main}>
-      <Picker characters={characters} objects={objects} />
-    </main>
-  );
-=======
 lingo.setup(
   Number(process.env.LINGO_SPACE_ID),
   process.env.LINGO_API_KEY as string
@@ -45,5 +19,4 @@ export default async function Home() {
   const objects = await getSectionAssets('Pww1EY');
 
   return <App data={{ characters, objects }} />;
->>>>>>> 7f48b21 (first commit for UI update)
 }
